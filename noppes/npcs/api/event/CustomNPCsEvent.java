aff97b9af721bccbc3ea62d155e043fd80938ffd
PopSlime <popslime@qq.com>
2023-10-11 22:20:10 +0800

    Add docs for `CustomNPCsEvent`.

--- noppes/npcs/api/event/CustomNPCsEvent.java
+++ noppes/npcs/api/event/CustomNPCsEvent.java
@@ -4,6 +4,12 @@ import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.Event;
 import noppes.npcs.api.NpcAPI;
 
+/**
+ * <p>Represents an event dispatched by CNPC to the scripts.</p>
+ */
 public class CustomNPCsEvent extends Event {
+	/**
+	 * <p>An instance of the {@link NpcAPI} class.</p>
+	 */
 	public final NpcAPI API = NpcAPI.Instance();
 }
