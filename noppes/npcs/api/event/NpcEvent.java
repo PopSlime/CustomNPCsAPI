da0cb82579e922015b795c65279131a248ee6c81
PopSlime <popslime@qq.com>
2023-10-12 10:37:33 +0800

    Cleanup docs.

--- noppes/npcs/api/event/NpcEvent.java
+++ noppes/npcs/api/event/NpcEvent.java
@@ -20,7 +20,6 @@ import noppes.npcs.api.entity.IPlayer;
 
 /**
  * <p>Represents an event triggered on NPCs.</p>
- *
  * <p>Must be listened in an NPC script.</p>
  */
 public class NpcEvent extends CustomNPCsEvent{
@@ -31,7 +30,6 @@ public class NpcEvent extends CustomNPCsEvent{
 
 	/**
 	 * <p>Creates an instance of the {@link NpcEvent} class.</p>
-	 *
 	 * <p>Scripters should not use this constructor.</p>
 	 */
 	public NpcEvent(ICustomNpc npc) {
@@ -39,16 +37,15 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 
 	/**
-	 * <p>Triggered when the NPC is created, edited, loaded, reset, or respawned.</p>
-	 *
-	 * <p>The hook function name of this event is <code>init</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>init</code><br />
+	 * Triggered when the NPC is created, edited, loaded, reset, or respawned.
+	 * </p>
 	 * <p>This event is not cancelable.</p>
 	 */
 	public static class InitEvent extends NpcEvent{
 		/**
 		 * <p>Creates an instance of the {@link InitEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public InitEvent(ICustomNpc npc) {
@@ -57,18 +54,16 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 	
 	/**
-	 * <p>Triggered every 10 game ticks (0.5 seconds expected.)</p>
-	 *
-	 * <p>The hook function name of this event is <code>tick</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>tick</code><br />
+	 * Triggered every 10 game ticks (0.5 seconds expected.)
+	 * </p>
 	 * <p>This event is first triggered 10 game ticks after the NPC is created or loaded. It keeps being triggered until the NPC is despawned or unloaded. Note that it also keeps being triggered after the NPC dies but with respawn enabled.</p>
-	 *
 	 * <p>This event is not cancelable.</p>
 	 */
 	public static class UpdateEvent extends NpcEvent{
 		/**
 		 * <p>Creates an instance of the {@link UpdateEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public UpdateEvent(ICustomNpc npc) {
@@ -77,23 +72,21 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 
 	/**
-	 * <p>Triggered when the NPC targets an entity.</p>
-	 *
-	 * <p>The hook function name of this event is <code>target</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>target</code><br />
+	 * Triggered when the NPC targets an entity.
+	 * </p>
 	 * <p>Canceling this event prevents the target of the NPC from being changed and the attack line of the NPC from being said.</p>
 	 */
 	@Cancelable
 	public static class TargetEvent extends NpcEvent{
 		/**
 		 * <p>The entity targeted.</p>
-		 *
 		 * <p>Setting this field will cause the NPC to target the set entity (or to lose target if set to <code>null</code>) instead.</p>
 		 */
 		public IEntityLiving entity;
 		/**
 		 * <p>Creates an instance of the {@link TargetEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public TargetEvent(ICustomNpc npc, LivingEntity entity) {
@@ -103,10 +96,10 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 	
 	/**
-	 * <p>Triggered when the NPC loses its target.</p>
-	 *
-	 * <p>The hook function name of this event is <code>targetLost</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>targetLost</code><br />
+	 * Triggered when the NPC loses its target.
+	 * </p>
 	 * <p>Canceling this event prevents the target of the NPC from being lost if possible.</p>
 	 */
 	@Cancelable
@@ -117,7 +110,6 @@ public class NpcEvent extends CustomNPCsEvent{
 		public final IEntityLiving entity;
 		/**
 		 * <p>Creates an instance of the {@link TargetLostEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public TargetLostEvent(ICustomNpc npc, LivingEntity entity) {
@@ -127,10 +119,10 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 	
 	/**
-	 * <p>Triggered when a player interacts (default control: mouse right click) with the NPC.</p>
-	 *
-	 * <p>The hook function name of this event is <code>interact</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>interact</code><br />
+	 * Triggered when a player interacts (default control: mouse right click) with the NPC.
+	 * </p>
 	 * <p>Canceling this event blocks the original interaction that the NPC would have done.</p>
 	 */
 	@Cancelable
@@ -141,7 +133,6 @@ public class NpcEvent extends CustomNPCsEvent{
 		public final IPlayer player;
 		/**
 		 * <p>Creates an instance of the {@link InteractEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public InteractEvent(ICustomNpc npc, Player player) {
@@ -151,10 +142,10 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 	
 	/**
-	 * <p>Triggered when the NPC dies.</p>
-	 *
-	 * <p>The hook function name of this event is <code>died</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>died</code><br />
+	 * Triggered when the NPC dies.
+	 * </p>
 	 * <p>This event is not cancelable.</p>
 	 */
 	public static class DiedEvent extends NpcEvent{
@@ -165,38 +156,32 @@ public class NpcEvent extends CustomNPCsEvent{
 		
 		/**
 		 * <p>The type of the damage source that causes the death.</p>
-		 *
 		 * <p>Identical to the value returned by calling {@link IDamageSource#getType()} on {@link #damageSource}.</p>
 		 */
 		public final String type;
 		/**
 		 * <p>The ultimate source entity of the damage that causes the death.</p>
-		 *
 		 * <p>For an indirect damage, this is the indirect source of the damage.</p>
 		 */
 		public final IEntity source;
 		
 		/**
 		 * <p>The items to be dropped from the NPC.</p>
-		 *
 		 * <p>Setting this field will change the dropped items.</p>
 		 */
 		public IItemStack[] droppedItems;
 		/**
 		 * <p>The amount of experiences to be dropped from the NPC.</p>
-		 *
 		 * <p>Setting this field will change the dropped amount of experiences.</p>
 		 */
 		public int expDropped;
 		/**
 		 * <p>The death line to be said by the NPC.</p>
-		 *
 		 * <p>Setting this field will change the said line (or blocks the line if set to <code>null</code>.)</p>
 		 */
 		public ILine line;
 		/**
 		 * <p>Creates an instance of the {@link DiedEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public DiedEvent(ICustomNpc npc, DamageSource damagesource, Entity entity) {
@@ -208,10 +193,10 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 	
 	/**
-	 * <p>Triggered when the NPC kills an entity.</p>
-	 *
-	 * <p>The hook function name of this event is <code>kill</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>kill</code><br />
+	 * Triggered when the NPC kills an entity.
+	 * </p>
 	 * <p>This event is not cancelable.</p>
 	 */
 	public static class KilledEntityEvent extends NpcEvent{
@@ -221,7 +206,6 @@ public class NpcEvent extends CustomNPCsEvent{
 		public final IEntityLiving entity;
 		/**
 		 * <p>Creates an instance of the {@link KilledEntityEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public KilledEntityEvent(ICustomNpc npc, LivingEntity entity) {
@@ -231,10 +215,10 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 	
 	/**
-	 * <p>Triggered when the NPC is going to do a melee attack.</p>
-	 *
-	 * <p>The hook function name of this event is <code>meleeAttack</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>meleeAttack</code><br />
+	 * Triggered when the NPC is going to do a melee attack.
+	 * </p>
 	 * <p>Canceling this event prevents the melee attack from being done.</p>
 	 */
 	@Cancelable
@@ -245,14 +229,12 @@ public class NpcEvent extends CustomNPCsEvent{
 		public final IEntityLiving target;
 		/**
 		 * <p>The amount of damage to be applied.</p>
-		 *
 		 * <p>Setting this field changes the amount of damage.</p>
 		 */
 		public float damage;
 		
 		/**
 		 * <p>Creates an instance of the {@link MeleeAttackEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public MeleeAttackEvent(ICustomNpc npc, LivingEntity target, float damage) {
@@ -263,10 +245,10 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 	
 	/**
-	 * <p>Triggered when the NPC does a ranged attack by shooting projectiles.</p>
-	 *
-	 * <p>The hook function name of this event is <code>rangedLaunched</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>rangedLaunched</code><br />
+	 * Triggered when the NPC does a ranged attack by shooting projectiles.
+	 * </p>
 	 * <p>This event is not cancelable.</p>
 	 */
 	public static class RangedLaunchedEvent extends NpcEvent{
@@ -276,20 +258,17 @@ public class NpcEvent extends CustomNPCsEvent{
 		public final IEntityLiving target;
 		/**
 		 * <p>The damage of every projectile shot.</p>
-		 *
 		 * <p>Setting this field has no effect.</p>
 		 */
 		public float damage;
 		/**
 		 * <p>The list of the projectiles shot.</p>
-		 *
 		 * <p>Modifying the list itself has no effect.</p>
 		 */
 		public List<IProjectile> projectiles = new ArrayList<IProjectile>();
 		
 		/**
 		 * <p>Creates an instance of the {@link RangedLaunchedEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public RangedLaunchedEvent(ICustomNpc npc, LivingEntity target, float damage) {
@@ -300,10 +279,10 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 	
 	/**
-	 * <p>Triggered when the NPC is damaged.</p>
-	 *
-	 * <p>The hook function name of this event is <code>damaged</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>damaged</code><br />
+	 * Triggered when the NPC is damaged.
+	 * </p>
 	 * <p>Canceling this event prevents the damage from being applied.</p>
 	 */
 	@Cancelable
@@ -314,13 +293,11 @@ public class NpcEvent extends CustomNPCsEvent{
 		public final IDamageSource damageSource;
 		/**
 		 * <p>The ultimate source entity of the damage that causes the death.</p>
-		 *
 		 * <p>For an indirect damage, this is the indirect source of the damage.</p>
 		 */
 		public final IEntity source;
 		/**
 		 * <p>The amount of damage to be applied.</p>
-		 *
 		 * <p>Setting this field changes the amount of damage.</p>
 		 */
 		public float damage;
@@ -331,7 +308,6 @@ public class NpcEvent extends CustomNPCsEvent{
 		
 		/**
 		 * <p>Creates an instance of the {@link DamagedEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public DamagedEvent(ICustomNpc npc, Entity source, float damage, DamageSource damagesource) {
@@ -343,10 +319,10 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 	
 	/**
-	 * <p>Triggered when the NPC collides with another entity.</p>
-	 *
-	 * <p>The hook function name of this event is <code>collide</code>.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>collide</code><br />
+	 * Triggered when the NPC collides with another entity.
+	 * </p>
 	 * <p>This event is not cancelable.</p>
 	 */
 	public static class CollideEvent extends NpcEvent{
@@ -357,7 +333,6 @@ public class NpcEvent extends CustomNPCsEvent{
 		
 		/**
 		 * <p>Creates an instance of the {@link CollideEvent} class.</p>
-		 *
 		 * <p>Scripters should not use this constructor.</p>
 		 */
 		public CollideEvent(ICustomNpc npc, Entity entity) {
@@ -367,13 +342,13 @@ public class NpcEvent extends CustomNPCsEvent{
 	}
 	
 	/**
-	 * <p>Triggered when a timer on the NPC is done.</p>
-	 *
-	 * <p>The hook function name of this event is <code>timer</code>.</p>
-	 *
-	 * <p>See {@link ICustomNpc#getTimers()} for more detail.</p>
-	 *
+	 * <p>
+	 * Hook function name: <code>timer</code><br />
+	 * Triggered when a timer on the NPC is done.
+	 * </p>
 	 * <p>This event is not cancelable.</p>
+	 *
+	 * @see ICustomNpc#getTimers()
 	 */
 	public static class TimerEvent extends NpcEvent{
 		/**
--- noppes/npcs/api/event/package-info.java
+++ noppes/npcs/api/event/package-info.java
@@ -1,18 +1,14 @@
 /**
  * <p>This package contains event classes whose instances are dispatched to the corresponding hook functions (if present) in CNPC scripts.</p>
- *
  * <p>These classes represent different events that a script may listen to, e.g. when a player is initialized, an NPC is damaged, etc. Most scripts are written in a way that they listen to specific events, and then do specific actions.</p>
- *
  * <p>To listen to an event in a script, a function with the hook function name of the corresponding event class and a parameter (usually named <code>event</code> or <code>e</code>) is written in the script. When the event is triggered, this function will be called with its only parameter set to an instance of the event class. For example, to listen to the {@link NpcEvent.InitEvent} event in an NPC script:</p>
- *
  *<pre>
  *function init(e) {
  *
  *}
  *</pre>
- *
  * <p>In this example, <code>init</code> is the hook function name of the {@link NpcEvent.InitEvent} event. When the scripted NPC is initialized, this function will be called, and its parameter <code>e</code> will be set to an instance of the {@link NpcEvent.InitEvent} event class, which contains information about this event.</p>
- *
+ * <p>For the hook function name of each events, check its description.</p>
  * <p>Event classes with different "prefixes" are triggered in different scriptable objects. See their descriptions for details.</p>
  */
 package noppes.npcs.api.event;
