580bbbb875ba42df11711efb9f4508c6cb1ef4e9
PopSlime <popslime@qq.com>
2023-11-17 13:53:13 +0800

    Add docs for `IMob`.

--- noppes/npcs/api/entity/IMob.java
+++ noppes/npcs/api/entity/IMob.java
@@ -3,31 +3,57 @@ package noppes.npcs.api.entity;
 import net.minecraft.world.entity.Mob;
 import noppes.npcs.api.IPos;
 
+/**
+ * <p>Represents a <a href="https://minecraft.wiki/w/Mob">mob</a>.</p>
+ *
+ * <p>Prior to 1.16.5-14Nov21snapshot, this interface is named <code>IEntityLiving</code>. It is renamed as <code>net.minecraft.entity.EntityLiving</code> is refactored to <code>net.minecraft.entity.MobEntity</code> (now refactored to {@link Mob}.)</p>
+ * <p>Prior to 1.8.9(29oct16), the interface is not generic.</p>
+ */
 public interface IMob<T extends Mob> extends IEntityLiving<T> {
 
 	/**
-	 * @return Whether or not this entity is navigating somewhere
+	 * <p>Gets whether the mob is navigating.</p>
+	 * @return Whether the mob is navigating.
 	 */
 	public boolean isNavigating();
 	
 	/**
-	 * Stop navigating wherever this npc was walking to
+	 * <p>Stops the current navigation on the mob.</p>
+	 * <p>Does nothing if the mob is not navigating.</p>
 	 */
 	public void clearNavigation();
 	
 	/**
-	 * Start path finding toward this target
-	 * @param x Destination x position
-	 * @param y Destination x position
-	 * @param z Destination x position
+	 * <p>Starts the navigation towards a target position.</p>
+	 * <p>The current navigation is stopped if present.</p>
+	 * @param x The X coordinate of the target position.
+	 * @param y The Y coordinate of the target position.
+	 * @param z The Z coordinate of the target position.
+	 * @param speed The speed multiplier.
+	 * <p>The movement speed during the navigation is modified to the <code>generic.movement_speed</code> attribute value of the mob multiplied by the value of <code>speed</code> parameter.</p>
 	 */
 	public void navigateTo(double x, double y, double z, double speed);
 
+	/**
+	 * <p>Makes the mob jump.</p>
+	 *
+	 * @since 1.10.2(21jul17)
+	 */
 	public void jump();
-
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public T getMCEntity();
-
+	
+	/**
+	 * <p>Gets the destination position of the current navigation.</p>
+	 * <p>The destination position may be a position different from, yet nearby, the target position specified in a {@link #navigateTo(double,double,double,double)} call.</p>
+	 * @return The destination position of the current navigation, or <code>null</code> if the mob is not navigating or there is no destination position.
+	 *
+	 * @since 1.12.2_05Jan19snapshot
+	 */
 	public IPos getNavigationPath();
 	
 }
